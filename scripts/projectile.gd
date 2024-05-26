9946ee503eca2d6250b45b7a8bf8437cce62bf6e
André Alija Ramos Agostini <andrealijaramosagostini@gmail.com>
2024-05-26 15:40:10 -0300

    Ataque feito

---
+++ scripts/projectile.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var velocity: Vector2 = Vector2(0,0)
+var duration = 20
+
+func _ready():
+	pass
+
+func _process(delta):
+	position += velocity * delta
+	duration -= delta
+	if duration <= 0:
+		queue_free();
+
+func _on_body_entered(body):
+	queue_free()
---
+++ scripts/projtectile_spawner.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var projectile = load("res://scenes/Attacks/Projectile/projectile.tscn")
+@onready var path = $movingPath
+@onready var path2 = $movingPath2
+
+var spawn_cooldown: float = 0
+var spawn_duration = 5
+# Called when the node enters the scene tree for the first time.
+func _ready(): 
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(delta):
+	if spawn_duration > 0 and spawn_cooldown <= 0:
+		spawn_cooldown = 0.1375
+		shoot()
+		
+	spawn_cooldown -= 1 * delta
+	spawn_duration -= 1 * delta
+	
+func shoot():
+	path.add_child.call_deferred(projectile.instantiate())
+	path2.add_child.call_deferred(projectile.instantiate())
+	
