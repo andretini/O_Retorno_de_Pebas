9e35a0efb88e70101d93e6bbfa5121ef714b2890
luana-akemi-sakurada <111025797+luana-akemi-sakurada@users.noreply.github.com>
2024-06-12 17:01:58 -0300

    dialogo e mapa

--- scripts/character.gd
+++ scripts/character.gd
@@ -3,7 +3,13 @@ extends CharacterBody2D
 @export_category("Variables")
 @export var _move_speed: float = 64.0
 
+var pebas_in_range = false
+
 func _physics_process(delta: float)-> void:
+	if pebas_in_range == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			DialogueManager.show_example_dialogue_balloon(load("res://main.dialogue"),"start")
+			return
 	_move()
 
 func _move() -> void:
@@ -14,3 +20,14 @@ func _move() -> void:
 	
 	velocity = _direction.normalized() * _move_speed
 	move_and_slide()
+
+
+
+func _on_area_2d_dialogue_body_entered(body):
+	if body.has_method("pebas_npc"):
+		pebas_in_range = true
+
+
+func _on_area_2d_dialogue_body_exited(body):
+	if body.has_method("pebas_npc"):
+		pebas_in_range = false
